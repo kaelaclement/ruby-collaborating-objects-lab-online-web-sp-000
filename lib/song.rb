da6f9db74b48a3a719b3a05258ea78283c18917f
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-01 06:51:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,14 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
+
+  def new_by_filename(filename)
+    data = filename.split(" - ")
+    song = self.new(data[1])
+    song.artist = data[0]
+  end
+
 end
