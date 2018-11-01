a4fdd70da2ec4177063fc8d71b0ff9227c1c1d05
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-01 06:55:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     @name = name
   end
 
-  def new_by_filename(filename)
+  def self.new_by_filename(filename)
     data = filename.split(" - ")
     song = self.new(data[1])
     song.artist = data[0]
